bf43f3b33b8f708aac4218195551c3b6c7463199
Lonewolf <lonewolf@localhost.localdomain>
2020-08-01 18:50:14 +0530

    media url fix

--- gdrive_stream/settings.py
+++ gdrive_stream/settings.py
@@ -118,4 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join('static'),)
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
